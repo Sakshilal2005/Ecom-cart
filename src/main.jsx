98a2a74b7e03221b326efc82d6a1b875205a43e7
Sakshilal2005 <sakshilal2005@gmail.com>
2026-06-12 17:27:41 +0530

    Fix GitHub Pages deployment

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,17 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { BrowserRouter, Outlet, Route, Routes } from 'react-router'
+import { HashRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/pages/Home.jsx'
 import Cart from './components/pages/Cart.jsx'
 import MainLayout from './components/common/MainLayout.jsx'
 
 createRoot(document.getElementById('root')).render(
-  
-   <BrowserRouter>
-      <Routes>
-        <Route element={<MainLayout/>}>
-        <Route path={'/'} element={<Home/>}/>
-        <Route path={'/cart'} element={<Cart/>}/>
-        </Route>
-      </Routes>
-   </BrowserRouter>
-  
+  <HashRouter>
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Route>
+    </Routes>
+  </HashRouter>
 )
